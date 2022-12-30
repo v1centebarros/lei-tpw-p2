7907bbec006e72da29608a128b6f7ea79a56a8d1
mariana <marianaandrade@ua.pt>
2022-12-30 04:08:40 +0000

    fix navbar com form search

--- frontend/src/app/cards/cards.component.ts
+++ frontend/src/app/cards/cards.component.ts
@@ -4,9 +4,6 @@ import { Component, Input, OnInit } from '@angular/core';
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css'],
-  template:`
-  <h6 class="category text-info">{{title}}</h6>
-  `
 })
 export class CardsComponent implements OnInit{
     @Input() title?: number;
@@ -15,6 +12,4 @@ export class CardsComponent implements OnInit{
   
     ngOnInit(): void {
     }
-  
-
 }
