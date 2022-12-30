4073b956bbd59986fd1cedde0017279b064847f5
mariana <marianaandrade@ua.pt>
2022-12-30 03:22:19 +0000

    transportar info entre componentes

--- frontend/src/app/cards/cards.component.ts
+++ frontend/src/app/cards/cards.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
+  template:`
+  <h6 class="category text-info">{{title}}</h6>
+  `
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit{
+    @Input() title?: number;
+    
+    constructor() { }
+  
+    ngOnInit(): void {
+    }
+  
 
 }
