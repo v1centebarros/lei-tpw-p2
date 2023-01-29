c5d6e6e7a21220ca8eec8074293eace64ad12700
afartur <art.afo@ua.pt>
2023-01-29 16:34:10 +0000

    Rating Starts in Book Details have the correct representation

--- backend2/core/views/book.py
+++ backend2/core/views/book.py
@@ -12,7 +12,7 @@ class BookFilter(django_filters.FilterSet):
 
     class Meta:
         model = Book
-        fields = ['year', 'title', 'publisher', 'language', 'rating']
+        fields = ['year', 'title', 'publisher', 'language', 'rating', 'author']
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
