ea793a9964a53eaeadd1ad97d1e6bc1908a0cd87
mariana <marianaandrade@ua.pt>
2023-01-30 01:17:40 +0000

    adicionar um autor aos favoritos

--- frontend/src/app/author-profile/author-profile.component.ts
+++ frontend/src/app/author-profile/author-profile.component.ts
@@ -4,6 +4,8 @@ import { Author } from '../models/author.model';
 import { AuthorService } from '../services/author.service';
 import { Book } from '../models/book.model';
 import { BookService } from '../services/book.service';
+import { AuthService } from '../services/auth/auth.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-author-profile',
@@ -14,17 +16,31 @@ export class AuthorProfileComponent implements OnInit {
   author: Author;
   books: Book[];
   image: any;
+  user: any;
+  logged: boolean = false;
+  text_fav: string = "Add Fav";
+  state_fav: boolean = false;
+
 
   constructor(
     private authorService: AuthorService,
     private bookService: BookService,
     private route: ActivatedRoute,
+    private authenticationService: AuthService,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
     this.getAuthor();
     this.getAuthorBooks();
     this.getImage();
+    if (this.authenticationService.loggedIn() && this.authenticationService.getUserInfo().type == "user"){
+      this.user = this.authenticationService.getUserInfo();
+      this.verifyFav();
+      this.logged = true;
+      console.log("!ola")
+    }
+   
   }
 
   getAuthor(): void {
@@ -57,6 +73,36 @@ export class AuthorProfileComponent implements OnInit {
     }
     else
       this.image = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+  }
+
+  addFav(): void {
+    if (this.user !== null && this.user.type == "user") {
+      if (this.state_fav) {
+        this.text_fav = "Add Fav";
+        this.userService.removeFavAuthor(this.user.id, this.author.id)
+          .subscribe(() => this.state_fav = false);
+      } else {
+        this.text_fav = "Remove Fav";
+        this.userService.addFavAuthor(this.user.id, this.author.id)
+          .subscribe(() => this.state_fav = true);
+      }
+    } else {
+      alert("You must be logged in to add a favorite");
+    }
+  }
+
+  verifyFav(): void {
+    if (this.user !== null) {
+      this.userService.getUserFavBook(this.user.id)
+        .subscribe(authors => {
+          for (let author of authors) {
+            if (author.id === this.author.id) {
+              this.state_fav = true;
+              this.text_fav = "Remove Fav";
+            }
+          }
+        });
+    }
   }
 
 }
