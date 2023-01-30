f5362667bf2072f178076c9d70dc95dff24f0ec7
mariana <marianaandrade@ua.pt>
2023-01-30 15:57:10 +0000

    end point publishers

--- backend2/core/views/publisher.py
+++ backend2/core/views/publisher.py
@@ -1,7 +1,29 @@
+from http.client import ResponseNotReady
+from urllib import response
 from rest_framework import viewsets
-from ..models import Publisher
-from ..serializers import PublisherSerializer
+from ..models import Publisher, Book
+from ..serializers import BookSerializer, PublisherSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class PublisherViewSet(viewsets.ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
+    
+
+    @action(detail=True, methods=['get'], name='Get Publisher Books')
+    def get_publisher_books(self,request, pk):
+        publisher = Publisher.objects.get(pk=pk)
+        books = Book.objects.all()
+        publisher_books = books.filter(publisher=publisher)
+        publisher_books_info = []
+        for book in publisher_books:
+            print(book)
+            info = BookSerializer(book).data
+            publisher_books_info.append(info)
+        return Response(publisher_books_info)
+
+
+
+        
