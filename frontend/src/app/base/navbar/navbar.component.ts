28fc7d37af83b0c8071a86bce4a715a4b40310a0
mariana <marianaandrade@ua.pt>
2023-01-30 00:53:22 +0000

    navbar linha de add book

--- frontend/src/app/base/navbar/navbar.component.ts
+++ frontend/src/app/base/navbar/navbar.component.ts
@@ -12,6 +12,7 @@ export class NavbarComponent  implements OnInit{
 
   user!: any;
   loggedIn: boolean;
+  typeAuthor: boolean;
 
   constructor(
     private router: Router, 
@@ -22,6 +23,7 @@ export class NavbarComponent  implements OnInit{
     console.log(this.authenticationService.loggedIn())
     this.loggedIn = this.authenticationService.loggedIn();
     this.user = this.authenticationService.getUserInfo();
+    this.typeAuthor = this.user.type == "author";
     this.getImage()
   }
 
